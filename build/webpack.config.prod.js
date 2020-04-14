4a4a829494d6dbf9bbc7cc5e8df126efd31ad46d
Huzedong <huzedong2008@vip.qq.com>
2020-04-15 00:12:37 +0800

    use rollup replace webpack

--- build/webpack.config.prod.js
+++
@@ -1,9 +0,0 @@
-const webpackConfigCommon = require("./webpack.config.common");
-
-module.exports = {
-	output: {
-		filename: `${process.env.npm_package_name}.js`,
-	},
-
-	...webpackConfigCommon,
-};
